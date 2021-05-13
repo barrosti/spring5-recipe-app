dc3e830bd8dcde604d4d154aca03422db44efd14
barrosti <barrosti@outlook.com>
2021-05-13 16:13:50 +0100

    Spring Data JPA Query Methods

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,13 @@
+-- CATEGORY
 INSERT INTO category (description) VALUES ('American');
 INSERT INTO category (description) VALUES ('Italian');
 INSERT INTO category (description) VALUES ('Mexican');
 INSERT INTO category (description) VALUES ('Fast Food');
+
+-- UNIT_OF_MEASURE
 INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
 INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (description) VALUES ('Cup');
 INSERT INTO unit_of_measure (description) VALUES ('Pinch');
 INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+
